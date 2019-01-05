4f3e0ad3860f11e44509a9cf248114f45d50888c
Mahdi <mahdikarknii@gmail.com>
2019-01-06 00:23:17 +0100

    consultation commit

--- src/app/services/consultation.service.ts
+++ src/app/services/consultation.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConsultationService {
-
+  cosultationTable = [
+    { id: 1, date: new Date, patientID: 1, notes: "figoo ligoo" },
+    { id: 2, date: new Date, patientID: 2, notes: "bagouette" },
+    { id: 3, date: new Date, patientID: 3, notes: "conesultationne" },
+    { id: 4, date: new Date, patientID: 4, notes: "baba sem7" },
+  ];
+  consultation = { id: null, date: null, patientID: null, notes: "" };
+  getConsultation() {
+    return this.cosultationTable;
+  }
+  addConsultation(cons) {
+    this.consultation.id = cons.id;
+    this.consultation.date = cons.date;
+    this.consultation.patientID = cons.patientID;
+    this.consultation.notes = cons.notes;
+  }
   constructor() { }
 }
