8dfdd768a284cf85402e95c99e3553bb8b0b333c
ChediDerbali <chederbali@gmail.com>
2019-01-06 14:30:13 +0100

    fixing popup

--- src/app/medecin/medecin.component.ts
+++ src/app/medecin/medecin.component.ts
@@ -1,6 +1,7 @@
 import { MedecinService } from './../services/medecin.service';
 import { OnInit, Component } from '@angular/core';
 import { Medecin } from '../models/medecin';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -14,7 +15,8 @@ export class MedecinComponent implements OnInit {
   profilMedecin: Medecin;
   profilMod = new Medecin();
   message = '';
-  constructor(public medecinService: MedecinService) {
+  modalRef: NgbModalRef;
+  constructor(public medecinService: MedecinService, private modalService: NgbModal) {
 
     this.profilMedecin = medecinService.getInfo();
 
@@ -23,4 +25,17 @@ export class MedecinComponent implements OnInit {
   ngOnInit() {
   }
 
+  open(content) {
+    this.modalRef = this.modalService.open(content, { centered: true, size: 'lg' });
+  }
+  close() {
+    this.modalRef.close('');
+  }
+  validate() {
+    this.medecinService.setProfil(this.profilMod);
+    console.log(this.profilMod);
+    this.profilMedecin = this.medecinService.getInfo();
+    this.profilMod = new Medecin();
+    this.close();
+  }
 }
