e57b457a67770d1737b15243deb931d17e53b1f6
Mahdi <mahdikarknii@gmail.com>
2019-01-06 14:35:28 +0100

    editing pop up modal for client profile

--- src/app/medecin/medecin.component.ts
+++ src/app/medecin/medecin.component.ts
@@ -1,6 +1,7 @@
 import { MedecinService } from './../services/medecin.service';
 import { OnInit, Component } from '@angular/core';
 import { Medecin } from '../models/medecin';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -14,7 +15,8 @@ export class MedecinComponent implements OnInit {
   profilMedecin: Medecin;
   profilMod = new Medecin();
   message = '';
-  constructor(public medecinService: MedecinService) {
+  modalRef:NgbModalRef
+  constructor(public medecinService: MedecinService, private modalService:NgbModal) {
 
     this.profilMedecin = medecinService.getInfo();
 
@@ -22,5 +24,13 @@ export class MedecinComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  open(content){
+    this.profilMod = this.medecinService.getInfo();
+    this.modalRef =  this.modalService.open(content, {centered: true, size:'lg'});
+   }
+   close(){
+     this.modalRef.close('');
+   }
 
 }
